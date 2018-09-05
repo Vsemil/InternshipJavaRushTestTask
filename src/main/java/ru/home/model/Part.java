7dc536ca52f7312b897662b18ab93f1f03c93367
Vsemil <i.razgulin@ya.ru>
2018-09-05 16:16:43 +0300

    поддержка JPA

--- src/main/java/ru/home/model/Part.java
+++ src/main/java/ru/home/model/Part.java
@@ -1,6 +1,14 @@
 package ru.home.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Part {
+
+    @Id
+    @GeneratedValue
     private long id;
     private String name;
     private boolean isCompulsory;
