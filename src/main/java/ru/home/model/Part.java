ce33f0d289e3b98833b9368ca94450f5f149c7b1
Vsemil <i.razgulin@ya.ru>
2018-09-05 15:29:37 +0300

    Начальная настройка

--- src/main/java/ru/home/model/Part.java
+++ src/main/java/ru/home/model/Part.java
@@ -1,4 +1,40 @@
 package ru.home.model;
 
 public class Part {
+    private long id;
+    private String name;
+    private boolean isCompulsory;
+    private int amount;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompulsory() {
+        return isCompulsory;
+    }
+
+    public void setCompulsory(boolean compulsory) {
+        isCompulsory = compulsory;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/ru/home/rest/PartRestController.java
+++ src/main/java/ru/home/rest/PartRestController.java
@@ -1,4 +1,15 @@
 package ru.home.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.home.model.Part;
+
+@RestController
 public class PartRestController {
+
+    @RequestMapping("/all")
+    public Part greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Part();
+    }
 }
